80c67edc3fb0c6888a2011233f45a72fb445d51b
skmbw <tongku2008@126.com>
2014-02-14 14:27:05 +0800

    属性配置类更改，增加或者属性的静态方法，已经类名更改

--- bizili/src/main/java/com/vteba/config/spring/EncryptPropHolder.java
+++ bizili/src/main/java/com/vteba/config/spring/EncryptPropHolder.java
@@ -1,5 +1,8 @@
 package com.vteba.config.spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 import com.vteba.util.cryption.DESUtils;
@@ -9,10 +12,11 @@ import com.vteba.util.cryption.DESUtils;
  * @author yinlei 
  * date 2012-7-16 下午4:28:33
  */
-public class EncryptPropHolderConfigurer extends PropertyPlaceholderConfigurer {
+public class EncryptPropHolder extends PropertyPlaceholderConfigurer {
 	private String[] encryptPropNames = { "jdbc.username", "jdbc.password" };
+	private static Map<String, String> propertiesMap = new HashMap<String, String>();
 	
-	public EncryptPropHolderConfigurer() {
+	public EncryptPropHolder() {
 		super();
 	}
 	
@@ -22,6 +26,7 @@ public class EncryptPropHolderConfigurer extends PropertyPlaceholderConfigurer {
 			String decryptValue = DESUtils.getDecryptString(propertyValue);
 			return decryptValue;
 		} else {
+			propertiesMap.put(propertyName, propertyValue);
 			return super.convertPropertyValue(propertyValue);
 		}
 		
@@ -34,5 +39,14 @@ public class EncryptPropHolderConfigurer extends PropertyPlaceholderConfigurer {
 			}
 		}
 		return false;
+	}
+	
+	/**
+	 * 获得配置的属性值
+	 * @param name 属性名
+	 * @return 属性值
+	 */
+	public static String get(String name) {
+		return propertiesMap.get(name);
 	}
 }
