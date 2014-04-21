39d9213c8d0a4be076d8aaca2880c6189be2d06f
skmbw <tongku2008@126.com>
2014-04-21 13:55:01 +0800

    删掉不用的工具类

---
+++ bizili/src/test/java/com/vteba/test/ForkJoinTest.java
@@ -0,0 +1,11 @@
+package com.vteba.test;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinTest {
+	public static void main(String[] args) {
+		ForkJoinPool forkJoinPool = new ForkJoinPool();
+		
+		
+	}
+}
---
+++ bizili/src/test/java/com/vteba/test/PrintTask.java
@@ -0,0 +1,27 @@
+package com.vteba.test;
+
+import java.util.concurrent.ForkJoinTask;
+
+public class PrintTask extends ForkJoinTask<String> {
+
+	private static final long serialVersionUID = -6790612547660653125L;
+
+	@Override
+	public String getRawResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setRawResult(String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean exec() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
