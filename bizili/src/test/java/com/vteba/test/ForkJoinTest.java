adee11e0a58d5bae79d13da14c0f00ee4873cd3a
skmbw <tongku2008@126.com>
2014-04-21 13:56:17 +0800

    fork join

--- bizili/src/test/java/com/vteba/test/ForkJoinTest.java
+++ bizili/src/test/java/com/vteba/test/ForkJoinTest.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ForkJoinPool;
 public class ForkJoinTest {
 	public static void main(String[] args) {
 		ForkJoinPool forkJoinPool = new ForkJoinPool();
-		
+		PrintTask printTask = new PrintTask();
+		forkJoinPool.submit(printTask);
 		
 	}
 }
