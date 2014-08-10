ab97a1f0a3dcb7b2f83e46150fcdbbc10ce792d0
skmbw <tongku2008@126.com>
2014-08-10 20:35:03 +0800

    方法名便更

--- bizili/src/main/java/com/vteba/cache/hibernate/memcached/xmemcached/Xmemcache.java
+++ bizili/src/main/java/com/vteba/cache/hibernate/memcached/xmemcached/Xmemcache.java
@@ -16,7 +16,6 @@ import com.vteba.cache.hibernate.memcached.MemcacheExceptionHandler;
 import com.vteba.cache.memcached.Element;
 import com.vteba.cache.memcached.Store;
 import com.vteba.cache.memcached.manager.XMemcachedManager;
-import com.vteba.utils.cryption.CryptUtils;
 
 /**
  * Hibernate二级缓存，使用xmemcache implementation。
@@ -71,7 +70,7 @@ public class Xmemcache implements Memcache {
 			//return this.memcachedClient.get(Arrays.asList(keys));
 			return getMultiInternal(keys);
 		} catch (Exception e) {
-			this.exceptionHandler.handleErrorOnGet(CryptUtils.join(keys, ", "), e);
+			this.exceptionHandler.handleErrorOnGet(keys.toString(), e);
 		}
 		return null;
 	}
